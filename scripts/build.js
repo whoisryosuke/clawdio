a8e1a03f6710836d75a92f37bfd2e2614c8f6069
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-07-11 12:21:51 -0700

    :rocket: module path fix maybe

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,7 @@
 import { execFileSync } from "child_process";
 import { copyFileSync, readdirSync } from "fs";
 import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const args = process.argv.slice(2); // Exclude the first two elements (node path and script path)
 
@@ -40,17 +41,34 @@ function releaseModules() {
   log("Starting release process 🚀");
 
   modules.forEach((moduleName) => {
-    // wasm-pack build --target web
-    const modulePath = `./modules/${moduleName}/pkg`;
+    // Get current directory equivalent for ES modules
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
 
-    execFileSync("npm publish --access public", {
-      cwd: modulePath,
-      stdio: "inherit", // show output in console
-      env: {
-        ...process.env,
-        NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN,
-      },
-    });
+    // Build the path from scripts/ to modules/
+    const modulePath = path.resolve(
+      __dirname,
+      "..",
+      "modules",
+      moduleName,
+      "pkg"
+    );
+    // const modulePath = `./modules/${moduleName}/pkg`;
+    log("Trying to releas module", moduleName, modulePath);
+
+    try {
+      execFileSync("npm", ["publish", "--access", "public"], {
+        cwd: modulePath,
+        stdio: "inherit", // show output in console
+        env: {
+          ...process.env,
+          NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN,
+        },
+      });
+      log("Successfully released module", moduleName);
+    } catch (e) {
+      log("Failed to release module", moduleName);
+    }
   });
 }
 
