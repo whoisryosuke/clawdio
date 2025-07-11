10c93765c1c4f89b95773c4f5ffc478effe45d65
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-07-11 11:19:32 -0700

    :hammer: monorepo setup

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,12 @@
 import { execFileSync } from "child_process";
 import { copyFileSync, readdirSync } from "fs";
-import path from "path";
+import path, { dirname } from "path";
+
+const args = process.argv.slice(2); // Exclude the first two elements (node path and script path)
+
+if (args.length > 0) {
+  console.log("First argument:", args[0]);
+}
 
 /**
  * This file handles building the Rust code into WASM and JS,
@@ -29,3 +35,26 @@ modules.forEach((moduleName) => {
 });
 
 log("Build completed successfully 👏🎉");
+
+function releaseModules() {
+  log("Starting release process 🚀");
+
+  modules.forEach((moduleName) => {
+    // wasm-pack build --target web
+    const modulePath = `./modules/${moduleName}/pkg`;
+
+    execFileSync("npm publish --access public", {
+      cwd: path.join(modulesDir, modulePath),
+      stdio: "inherit", // show output in console
+      env: {
+        ...process.env,
+        NODE_AUTH_TOKEN: process.env.NODE_AUTH_TOKEN,
+      },
+    });
+  });
+}
+
+// Handle release
+if (args.length > 0 && args[0] == "--release") {
+  releaseModules();
+}
