8f2edd8244cda70e33b8fe77f896322c3505fba3
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-07-20 01:40:00 -0700

    :fire: no need

--- modules/test-helpers/src/lib.rs
+++ modules/test-helpers/src/lib.rs
@@ -4,7 +4,3 @@ pub mod plotting;
 pub fn generate_silence_samples() -> Vec<f32> {
     vec![0.0; 128]
 }
-
-// Re-export for convenience (optional)
-#[cfg(any(test, feature = "viz"))]
-pub use plotting::*;
