819da38eb9b25073f521360a608f08eab85a4835
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-07-21 15:30:55 -0700

    :zap: actual test for bitcrusher

--- modules/bitcrusher/tests/general.rs
+++ modules/bitcrusher/tests/general.rs
@@ -9,6 +9,24 @@ fn test_signal_effect() {
     let input = generate_sine_wave(num_samples);
     let output = filter.process_vec(input, 0.420);
 
+    // Bitcrusher creates a step-like pattern out of input signal
+    // Technically the modified signal will be equal sometimes, but mostly not
+    // So we check that (mostly) every point in signal is different
+    let mut is_equal = 0;
+    let mut is_diff = 0;
+    for index in 0..original.len() {
+        let sample_original = original[index];
+        let sample_effect = output[index];
+
+        if sample_original != sample_effect {
+            is_diff += 1;
+        } else {
+            is_equal += 1;
+        }
+    }
+    assert!(is_diff > is_equal, "Values should be less equal");
+
+
     // Visualize signal
     plot_waveform_comparison(
         &output,
