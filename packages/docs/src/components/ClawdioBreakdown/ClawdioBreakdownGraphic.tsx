6ac04ed687acb2807efdea219e7d6b8dfa589ac8
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-07-31 11:39:49 -0700

    :lipstick: mobile optimss

--- packages/docs/src/components/ClawdioBreakdown/ClawdioBreakdownGraphic.tsx
+++ packages/docs/src/components/ClawdioBreakdown/ClawdioBreakdownGraphic.tsx
@@ -5,6 +5,13 @@ import "./ClawdioBreakdown.css";
 import Title from "../ui/Title/Title";
 import Heading from "../ui/Heading/Heading";
 import Stack from "../ui/Stack/Stack";
+import {
+  BiCog,
+  BiExtension,
+  BiFile,
+  BiFileBlank,
+  BiRightArrowAlt,
+} from "react-icons/bi";
 
 const Subheader = ({ children }) => (
   <Heading as="h3" thin style={{ color: "var(--gray-8)" }}>
@@ -16,33 +23,52 @@ type Props = {};
 
 const ClawdioBreakdownGraphic = (props: Props) => {
   return (
-    <Stack className="ClawdioBreakdownGraphic_Container" horizontal>
-      <div>
-        <Title as="h3">Your Audio</Title>
-        <SineWave className="ClawdioBreakdownGraphic_Wave_Left" />
+    <Stack className="ClawdioBreakdownGraphic_Container">
+      <Stack className="SignalContainer">
+        <Stack>
+          <Title as="h3">Your Audio</Title>
+          <SineWave
+            className="ClawdioBreakdownGraphic_Wave_Left"
+            width="100%"
+          />
+        </Stack>
         <Subheader>Main CPU Thread</Subheader>
-      </div>
+      </Stack>
       <div>
         <div className="ClawdioBreakdownGraphic_Box">
           <Title as="h3">AudioWorkletNode</Title>
-          <Stack horizontal>
-            <Heading as="h3" thin>
-              Rust
-            </Heading>
-            <Heading as="h3" thin>
-              WASM
-            </Heading>
+          <Stack horizontal style={{ alignItems: "center" }}>
+            <Stack centered gap="0.25rem">
+              <BiFileBlank className="icon" color="var(--clawdio-1100)" />
+              <Heading as="h3" thin>
+                Rust
+              </Heading>
+            </Stack>
+            <BiRightArrowAlt className="icon" color="var(--gray-8)" />
+            <BiCog className="icon" color="var(--gray-11)" />
+            <BiRightArrowAlt className="icon" color="var(--gray-8)" />
+            <Stack centered gap="0.25rem">
+              <BiExtension className="icon" color="var(--violet-8)" />
+              <Heading as="h3" thin>
+                WASM
+              </Heading>
+            </Stack>
           </Stack>
         </div>
 
         <Subheader>Separate CPU Thread</Subheader>
       </div>
-      <div>
-        <Title as="h3">Audio + Effects</Title>
-        <SineWaveEffected className="ClawdioBreakdownGraphic_Wave_Right" />
+      <Stack className="SignalContainer">
+        <Stack>
+          <Title as="h3">Audio + Effects</Title>
+          <SineWaveEffected
+            className="ClawdioBreakdownGraphic_Wave_Right"
+            width="100%"
+          />
+        </Stack>
 
         <Subheader>Main CPU Thread</Subheader>
-      </div>
+      </Stack>
     </Stack>
   );
 };
