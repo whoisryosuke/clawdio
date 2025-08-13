4a10c0d760b71ec7e7dad7e0827664392ecd03ae
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-08-13 14:52:09 -0700

    :bug: force lenis resize on page change

--- packages/docs/src/theme/Root.tsx
+++ packages/docs/src/theme/Root.tsx
@@ -1,7 +1,22 @@
-import React from "react";
-import { ReactLenis } from "lenis/react";
+import React, { useEffect } from "react";
+import { ReactLenis, useLenis } from "lenis/react";
+
+import { useLocation } from "@docusaurus/router";
 
 function Root({ children }) {
+  const lenis = useLenis();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (lenis) {
+      // Small delay to ensure DOM is updated
+      setTimeout(() => {
+        lenis.resize();
+        lenis.scrollTo(0, { immediate: true });
+      }, 100);
+    }
+  }, [location.pathname, lenis]);
+
   return <ReactLenis root>{children}</ReactLenis>;
 }
 
